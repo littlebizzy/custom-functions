d70e946f78943e54514cd0e358d8e373ae48b933
Pau <pauiglesias@gmail.com>
2018-07-29 21:56:17 +0200

    First client side ajax interface.

--- assets/admin.js
+++ assets/admin.js
@@ -9,8 +9,38 @@ jQuery(document).ready(function($) {
 
 
 	$('#custom-functions-template-button').click(function() {
-		$(this).closest('.submit').find('.spinner').css('visibility', 'visible');
-		$(this).attr('disabled', true)
+
+		var $button = $(this);
+		$button.closest('.submit').find('.spinner').css('visibility', 'visible');
+		$button.attr('disabled', true)
+
+
+		var data = {
+			nonce: cstmfn_data['nonce'],
+			action: 'cstmfn_update',
+			code: $('#newcontent').val()
+		};
+
+		$.post(ajaxurl + '?_=' + new Date().getTime(), data, function(e) {
+
+			if ('undefined' == typeof e.status) {
+				alert('Unknown error');
+
+			} else if ('error' == e.status) {
+				alert(e.reason);
+
+			} else if ('ok' == e.status) {
+				alert('ok');
+			}
+
+		}).fail(function() {
+			alert('Server communication error.' + "\n" + 'Please try again after few moments.');
+
+		}).always(function() {
+			$button.closest('.submit').find('.spinner').css('visibility', 'hidden');
+			$button.attr('disabled', false);
+		});
+
 		return false;
 	});
 
